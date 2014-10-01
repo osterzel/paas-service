25a005111b85b290bfb8675ac21b518253d9ccf5
Oliver Sterzel <oliver@cloudsurge.co.uk>
2014-10-01 14:01:02 +0100

    Removed common components

--- api/tests/test_cluster.py
+++ api/tests/test_cluster.py
@@ -2,19 +2,27 @@ __author__ = 'oliver'
 
 import unittest
 import sys
+import flask
 
 sys.path.append( '../' )
 
 from mock import patch, MagicMock
 
 import api
+from api.resources.cluster import *
 
-class TestCustomers(unittest.TestCase):
+class TestCluster(unittest.TestCase):
 
     def setUp(self):
         self.data = "123"
+        self.something = ClusterCollection()
+        self.app = flask.app
+
+    def tearDown(self):
+        pass
 
     def test_get_customers(self):
         data = "123"
+        self.something.get()
 
         self.assertEqual("123",data)
