b63b3af9a768626ab86f039158368bfd50a53192
Oliver Sterzel <oliver@cloudsurge.co.uk>
2015-02-04 21:46:19 +0000

    Start of a basic client

---
+++ client/tests/test_client.py
@@ -0,0 +1,17 @@
+import unittest
+import requests
+import paasclient
+
+from mock import patch, MagickMock
+
+class TestClient(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_controller(self):
+        pass
+
+
+
+		
