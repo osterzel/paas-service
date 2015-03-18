b94b283ea1667166be6827d758d6f5946becb610
Oliver Sterzel <oliver@cloudsurge.co.uk>
2015-03-18 06:36:12 +0000

    Updated configure script

--- scripts/configure-services.sh
+++ scripts/configure-services.sh
@@ -21,8 +21,22 @@ fi
 
 if [ "$RABBITMQ" == "" ]
 then
-	docker run -p 5672:5672 -p 15672:15672 --name rabbitmq -d tutum/rabbitmq
+	RABBITMQ=`docker run -p 5672:5672 -p 15672:15672 --name rabbitmq -d tutum/rabbitmq`
+	sleep 5
 	#Now get password and setup rabbitmq
+	USER_DETAILS=""
+	while [ "$USER_DETAILS" == "" ]
+	do
+		USER_DETAILS=`docker logs $RABBITMQ | grep curl | awk '{print $3}'`
+		sleep 2
+	done
+	curl -XPUT -u $USER_DETAILS -H 'Content-type: application/json' http://localhost:15672/api/users/paas -d '{ "password": "paas", "tags": "administator" }'
+	curl -XPUT -u $USER_DETAILS -H 'Content-type: application/json' http://localhost:15672/api/vhosts/paas
+	curl -XPUT -u $USER_DETAILS -H 'Content-type: application/json' http://localhost:15672/api/permissions/paas/paas -d '{"scope":"paas","configure":".*","write":".*","read":".*"}'
+	curl -XPUT -u $USER_DETAILS -H 'Content-type: application/json' http://localhost:15672/api/permissions/paas/admin -d '{"scope":"paas","configure":".*","write":".*","read":".*"}'
+	curl -XPUT -u $USER_DETAILS -H 'Content-type: application/json' http://localhost:15672/api/exchanges/paas/paas -d '{"type":"topic","auto_delete":true,"durable":true,"arguments":[]}'
+	
+
 fi
 
 if [ "$CONTROLLER" != "" ]
